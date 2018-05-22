fec8f93478c9649c3f08d5c047df70447087f79f
Tyler <tylerm15@gmail.com>
2018-05-22 12:31:36 -0600

    initial seed files

---
+++ seed.groovy
@@ -0,0 +1,30 @@
+import groovy.sql.Sql
+import java.sql.*
+import com.mysql.jdbc.*
+
+println "waddup"
+// def mysqlJar = 
+// new File('.').eachFile(FILES) {
+//     println it.name
+// }
+// scriptDir = new File(getClass().protectionDomain.codeSource.location.path).parent
+// println scriptDir
+// println "open jar file"
+// URL url = new URL('file:' + scriptDir + '/mysqlconnect.jar');
+// def mysqlJarURL = mysqlJar.toURI().toURL()
+// println mysqlJarURL
+// def myurl = new URL('file:var/lib/jenkins/workspace/base/mysqlconnect.jar')
+// println myurl
+// InputStream stream = streamFileFromWorkspace('/var/lib/jenkins/workspace/base/mysqlconnect.jar')
+// this.class.classLoader.rootLoader.addURL(myurl)
+println "classpath loaded"
+hudson.FilePath workspace = hudson.model.Executor.currentExecutor().getCurrentWorkspace()
+println workspace
+
+
+def sql = Sql.newInstance('jdbc:mysql://10.20.20.66:3306/re_fsbo_front', 'tyler', 'dz77GAi7980', 'com.mysql.jdbc.Driver')
+
+sql.eachRow('SELECT Instance, Group_ID, Robot_Name FROM re_fsbo_init LIMIT 50') { row ->
+    println "${row.Instance}, ${row.Group_ID}, ${row.Robot_Name}"
+
+}
