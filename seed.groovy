1c6bae2db648af18127f92758687ee7fd07316c5
len0rd <tylerm15@gmail.com>
2018-08-14 11:55:05 -0400

    idk wtf my deal was before, but here's a working mysql conneciton..

--- seed.groovy
+++ seed.groovy
@@ -1,36 +1,9 @@
+import groovy.grape.Grape
+@Grab('mysql:mysql-connector-java:5.1.46')
+@GrabConfig(systemClassLoader = true)
+ 
 import groovy.sql.Sql
-import java.sql.*
-// import com.mysql.jdbc.*
-import org.apache.commons.text.WordUtils
 
-String test = "waddup ma dude"
-println test
-println WordUtils.capitalizeFully(test)
-// def mysqlJar = 
-// new File('.').eachFile(FILES) {
-//     println it.name
-// }
-// scriptDir = new File(getClass().protectionDomain.codeSource.location.path).parent
-// println scriptDir
-// println "open jar file"
-// URL url = new URL('file:' + scriptDir + '/mysqlconnect.jar');
-// def mysqlJarURL = mysqlJar.toURI().toURL()
-// println mysqlJarURL
-// def myurl = new URL('file:var/lib/jenkins/workspace/base/mysqlconnect.jar')
-// println myurl
-// InputStream stream = streamFileFromWorkspace('/var/lib/jenkins/workspace/base/mysqlconnect.jar')
-// this.class.classLoader.rootLoader.addURL(myurl)
-println "classpath loaded"
-// hudson.FilePath workspace = hudson.model.Executor.currentExecutor().getCurrentWorkspace()
-// println workspace
+Sql.withInstance("jdbc:mysql://localhost:3306/re_stat?useSSL=false", "tyler","12345", "com.mysql.jdbc.Driver") { sql ->
 
-Class.forName('com.mysql.jdbc.Driver').newInstance()
-
-Connection conn = DriverManager.getConnection('jdbc:mysql://10.20.20.66:3306/re_fsbo_front', 'tyler', 'dz77GAi7980')
-
-// def sql = Sql.newInstance('jdbc:mysql://10.20.20.66:3306/re_fsbo_front', 'tyler', 'dz77GAi7980', 'com.mysql.jdbc.Driver')
-
-// sql.eachRow('SELECT Instance, Group_ID, Robot_Name FROM re_fsbo_init LIMIT 50') { row ->
-//     println "${row.Instance}, ${row.Group_ID}, ${row.Robot_Name}"
-
-// }
+}
